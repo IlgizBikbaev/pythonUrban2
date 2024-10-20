b0d30e31825e1703a974446a7915eb14caa37990
IlgizBikbaev <131395377+IlgizBikbaev@users.noreply.github.com>
2024-10-20 12:55:00 +0300

    Add files via upload

--- module_2_hw1.py
+++ module_2_hw1.py
@@ -3,7 +3,7 @@ second = int(input("Введите целое трезначное число: "
 third =  int(input("Введите целое трезначное число: "))
 
 if first == second == third:
- print('3 совпадения')
+ print("3 совпадения")
 elif first == second or second == third or first == third:
  print("2  совпадения")
 else:
