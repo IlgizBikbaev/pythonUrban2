8fd6693088a27811a7e57c9fba15c257bb234cb6
IlgizBikbaev <131395377+IlgizBikbaev@users.noreply.github.com>
2024-10-25 00:01:50 +0300

    Add files via upload

---
+++ module_2_hw3.py
@@ -0,0 +1,20 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+a = len(numbers) + 1
+
+for i in numbers:
+    k = 0
+    if i == 1:
+        continue
+    for j in range(1, i + 1):
+        if i % j == 0 and i != j and j != 1:
+            k += 1
+            not_primes.append(i)
+            break
+    if k == 0:
+        primes.append(i)
+
+print('Простые числа: ', primes)
+print('Составные числа: ', not_primes)
+
