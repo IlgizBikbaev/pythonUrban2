9d76a67d13d07c5756d059a2344b99e1e2e0edd8
IlgizBikbaev <131395377+IlgizBikbaev@users.noreply.github.com>
2024-11-09 14:55:42 +0300

    Add files via upload

--- task1.py
+++ task1.py
@@ -1,31 +1,22 @@
 import random
 
-from pyexpat.errors import codes
-
+result = ''
 
 def stone_field():
-    stones = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-    stone_first = random.choice(stones)
-    return stone_first
+    number = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    number = random.choice(number)
+    return number
 
-stone_first = stone_field()
-print(stone_first)
+number = stone_field()
+print('Ваше число:', number)
 
-def get_key(stone_first):
-    key_dict = {
-        3: 12, 4: 13, 5: 1423, 6: 121524, 7: 162534, 8: 13172635,
-        9: 1218273645, 10: 141923283746, 11: 11029384756,
-        12: 12131511124210394857, 13: 112211310495867,
-        14: 1611325212343114105968, 15: 1214114232133124115106978,
-        16: 1317115262143531341251161079,
-        17: 11621531441351261171089,
-        18: 12151811724272163631545414513612711810,
-        19: 118217316415514613712811910,
-        20: 13141911923282183731746416515614713812911
-    }
+for i in range(1, number):
+    for j in range(i+1, number):
+        if i >= j:
+            continue
+        if number % (i+j) == 0:
+            result += str(i) + str(j)
 
-    key = key_dict.get(stone_first)
-    return key
+print('необходимый код:', result)
 
-key = get_key(stone_first)
-print(key)
+
